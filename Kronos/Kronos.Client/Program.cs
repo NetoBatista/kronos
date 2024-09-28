695c0472021d6fa37de8aa6f380c2a84b167ec6b
Neto Batista <netosantosbatista@outlook.com>
2024-09-28 20:08:47 -0300

    add dockerfile fix base address url

--- Kronos/Kronos.Client/Program.cs
+++ Kronos/Kronos.Client/Program.cs
@@ -1,10 +1,7 @@
-using Kronos.Client.Extension;
 using Kronos.Provider;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
-ConfigurationExtension.ConfigureKronosAPI(builder.Configuration);
 
 builder.Services.AddSingleton<DialogProvider>();
 
